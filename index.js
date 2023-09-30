da6bad1facc8599d1dcaf612db305e300090da62
congthang12312 <80143584+congthang12312@users.noreply.github.com>
2023-09-30 15:06:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-
+const version = process.env.APP_VERSION || 1
+const env = process.env.APP_ENV || "test"
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = `Hello Node!\n ${version} - ${env} `
   res.end(msg);
 });
 
